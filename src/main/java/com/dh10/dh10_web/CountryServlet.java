11db7c12472c60076d4348234be27d8e1dd26bae
Andrei Lazar <andreilazar.dev@gmail.com>
2021-10-27 17:10:52 +0200

    first commit

---
+++ src/main/java/com/dh10/dh10_web/CountryServlet.java
@@ -0,0 +1,28 @@
+package com.dh10.dh10_web;
+
+import com.dh10.stringchecker.CountryChecker;
+import com.dh10.stringchecker.StringChecker;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "countryServlet", value = "/countryservlet")
+public class CountryServlet extends HttpServlet {
+    public void init() {}
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String message = "non trovata corrispondeza";
+        StringChecker countrycheck = new CountryChecker();
+        String country = request.getParameter("country");
+        if(countrycheck.find_standard(country))
+            message = "Nazione Trovata";
+        message = "non trovata corrispondeza";
+        PrintWriter writer = response.getWriter();
+        writer.println("<h1>Country" + country + message + "</h1>");
+        writer.close();
+    }
+
+    public void destroy() {
+    }
+}
