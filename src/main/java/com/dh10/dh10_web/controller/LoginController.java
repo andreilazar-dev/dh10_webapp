8edef48ed53eb98a90d886709c669f1abfbb12e2
Andrei Lazar <andreilazar.dev@gmail.com>
2021-10-28 17:15:45 +0200

    fix issue

--- src/main/java/com/dh10/dh10_web/controller/LoginController.java
+++ src/main/java/com/dh10/dh10_web/controller/LoginController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String init(Model model) {
         model.addAttribute("msg", "Please Enter Your Login Details");
         return "login";
